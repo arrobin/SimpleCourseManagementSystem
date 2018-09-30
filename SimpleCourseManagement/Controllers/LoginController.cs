3bc42f0e7510e13b5297cff1793580809326cb2d
arrobin <arrobin02@gmail.com>
2018-09-30 22:54:05 +0600

    add dbcontext, add login controller,login view, give user access

---
+++ SimpleCourseManagement/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using SimpleCourseManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleCourseManagement.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            if (Session["UserDetailsId"] == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        [HttpPost]
+        public ActionResult Autherize(UserDetail aUser)
+        {
+            using (SimpleCourseManagementDbEntities db = new SimpleCourseManagementDbEntities())
+            {
+                var userDetails = db.UserDetails.FirstOrDefault(a => a.LoginName == aUser.LoginName && a.LoginPassword == aUser.LoginPassword);
+                if (userDetails == null)
+                {
+                    @ViewBag.Message = "Invalid User Name or Password";
+                    return View("Index", aUser);
+                }
+                else
+                {
+                    Session["UserDetailsId"] = userDetails.UserDetailsId;
+                    Session["UserName"] = userDetails.UserName;
+                    Session["LoginName"] = userDetails.LoginName;
+                    Session["UserRoleId"] = userDetails.UserRoleId;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
